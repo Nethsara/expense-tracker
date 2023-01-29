cf3e47fbd3b1c96ce308bcf4a2559ad3fb7a3da0
Nethsara Siyum <nethsara@apium.io>
2023-01-29 14:57:33 +0530

    Basic Structure

---
+++ src/context/GlobalState.js
@@ -0,0 +1,27 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+const initialState = {
+  transactions: [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 2, text: "Salary", amount: 300 },
+    { id: 3, text: "Book", amount: -10 },
+    { id: 4, text: "Camera", amount: 150 },
+  ],
+};
+
+export const TransactionContext = createContext(initialState);
+
+export const TransactionProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <TransactionContext.Provider
+      value={{
+        transactions: state.transactions,
+      }}
+    >
+      {children}
+    </TransactionContext.Provider>
+  );
+};
